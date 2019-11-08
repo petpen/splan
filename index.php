e018984338accba330f4f76903e87bb4ed85d4c5
Peter Pensold <git@pensold.net>
2019-11-08 10:46:53 +0100

    Added ability to edit names directly

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
+$default_names = "Alvin\nBernd\nChristian\nDaniel\nEmil";
+
 //Analyse input
 $year_in = isset($_GET['year']) ? sprintf('%d',$_GET['year']) : date('Y')+1;
 $month_begin_in = isset($_GET['monthb']) ? sprintf('%d',$_GET['monthb']) : 1;
 $month_end_in = isset($_GET['monthe']) ? sprintf('%d',$_GET['monthe']) : 12;
+$names_in = isset($_GET['names']) ? sprintf('%s',$_GET['names']) : $default_names;
 
 $year = ($year_in > 2000 && $year_in < 2100) ? $year_in : date('Y');
 $month_begin = ($month_begin_in > 1 && $month_begin_in <= 12) ? $month_begin_in : 1;
 $month_end = ($month_end_in >= $month_begin && $month_end_in <= 12) ? $month_end_in : 12;
+
+//Prepare names
+$names = explode("\n",$names_in);
 
 //Start script
 require_once("HolidaySchedule.php");
@@ -14,7 +20,6 @@ require_once("HolidaySchedule.php");
 //setlocale(LC_TIME, "de_DE");
 setlocale(LC_ALL, 'de_DE@euro', 'de_DE', 'deu_deu');
 
-$names = array('Alvin','Bernd','Christian','Daniel','Emil','Fred','Guy','Henry','Ian','John','Kurt');
 $shifts = 'FFFFFFFSSSSS  NNNNNNN       ';
  //' ' = Free, F = Morning, S = Late, N = Night
 $shift_start_day = mktime(0,0,0,1,24,2011); //Problems between April and September!
@@ -24,7 +29,7 @@ $schedule = new HolidaySchedule($year, array($month_begin,$month_end), $shifts, 
 $month_names = array('Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns='http://www.w3.org/1999/xhtml'>
+<html xmlns='http://www.w3.org/1999/xhtml' lang="de" xml:lang="de">
 <head>
 <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 <title>Schichtplan <?php echo $year;?></title>
@@ -66,7 +71,13 @@ $month_names = array('Januar','Februar','März','April','Mai','Juni','Juli','Aug
 				</td>
 			</tr>
             <tr>
-            	<td colspan="2" style="font-size:0.9em;">F = Früh, S = Spät, N = Nacht, '&nbsp;&nbsp;' = Frei</td>
+            	<td colspan="2" style="font-size:0.9em;">
+            	    <div class="shifts">F = Früh, S = Spät, N = Nacht, '&nbsp;' = Frei</div>
+            	    <div>
+                        <label for="names">Personen: </label>
+                        <textarea class="names" name="names" id="names"><?php echo join("\n", $names); ?></textarea>
+            	    </div>
+            	 </td>
             	<td align="right" valign="bottom" style="height:40px;">
 		            <input type="submit" value="Anzeigen" />
 		            <input type="button" onClick="javascript: window.print()" value="Drucken"/>
